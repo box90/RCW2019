9083e73c528fd8169e9117cf57ddf12df4c3ab87
Marco Bossi <marco.bossi@reti.it>
2020-01-07 17:54:11 +0100

    added Hub

--- RCW2019.API/Controllers/MatchesController.cs
+++ RCW2019.API/Controllers/MatchesController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using RWC2019.BL;
 using RWC2019.Entities;
 
@@ -12,22 +13,38 @@ namespace RCW2019.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class MatchesController : ControllerBase
     {
+
+        private IHubContext<Hubs.RCWHub> _hub;
+
+        public MatchesController(IHubContext<Hubs.RCWHub> hub)
+        {
+            _hub = hub;
+        }
+
         [HttpGet]
+        [Authorize]
         public async Task<List<Match>> GetMatches()
         {
-            return await MatchesManager.GetMatchesAsync();
+            var matches = await MatchesManager.GetMatchesAsync();
+            await _hub.Clients.All.SendAsync("GetMatches",matches);
+
+            return matches;
         }
 
         [HttpGet("{id}"), Authorize]
+        [Authorize]
         public async Task<MatchDetail> GetMatchDetailAsync(int id)
         {
-            return await MatchesManager.GetMatchDetailAsync(id);
+            var match = await MatchesManager.GetMatchDetailAsync(id);
+            //await _hub.Clients.All.SendAsync("GetMatchDetailAsync", match);
+
+            return match;
         }
 
         [HttpPut("{id}")]
+        [Authorize]
         public async Task<bool> StartMatch(int id)
         {
             return await MatchesManager.StartMatchAsync(id);
